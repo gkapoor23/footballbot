d0c2a077cbb8371458cce974fbc7ad2d7029c5c9
gkapoor23 <118529671+gkapoor23@users.noreply.github.com>
2023-03-19 21:15:15 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "game.h"
+
+int main() {
+    // Load teams and players data from /data folder
+    // Instantiate teams and players
+    // Add players to the teams
+
+    Game game(team1, team2);
+    game.play();
+
+    return 0;
+}
